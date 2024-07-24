83cddd3c0f5c5424aedb704c2f82f7ba51386458
CodyTVWeber <cody.tv.weber@protonmail.com>
2024-07-23 21:51:51 -0500

    Solved project 2 with python

--- problems/problem-00002/app.py
+++ problems/problem-00002/app.py
@@ -14,23 +14,23 @@ class Solution:
         num2 = str(l2.val)
 
         cur_node = l1
-        while cur_node.next:
-            num1 = num1 + str(cur_node.val)
+        while cur_node.next is not None:
             cur_node = cur_node.next
+            num1 = str(cur_node.val) + num1
 
         cur_node = l2
-        while cur_node.next:
-            num2 = num2 + str(cur_node.val)
-            cur_node.val = cur_node.next
+        while cur_node.next is not None:
+            cur_node = cur_node.next
+            num2 = str(cur_node.val) + num2
 
         num3 = int(num1) + int(num2)
         num3_string = str(num3)
-        split_num3 = num3_string.split('')
+        split_num3 = list(num3_string)
         nodes = [ListNode(int(x)) for x in reversed(split_num3)]
         nodes_length = len(nodes)
         for nodeIndex in range(nodes_length):
             cur_node = nodes[nodeIndex]
-            if nodeIndex < nodes_length:
-                cur_node.val = nodes[nodeIndex + 1]
+            if nodeIndex + 1 < nodes_length:
+                cur_node.next = nodes[nodeIndex + 1]
 
         return nodes[0]
